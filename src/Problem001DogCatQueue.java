e931c7e703a0c9c51285936436a43165483a4520
godwinbits <godwinbits@gmail.com>
2018-02-03 15:04:02 +0530

    Adding problem 1 file to repo

---
+++ src/Problem001DogCatQueue.java
@@ -0,0 +1,12 @@
+/**
+ * Created by gselvam on 2/3/18.
+ * #Queue
+ *
+ * An animal shelter, which holds only dogs and cats, operates on a strictly "First in, first out" basis. People must adopt either the
+ * "oldest" (based on arrival time) of all animals at the shelter, or they can select whether they would prefer a dog or a cat ( and will
+ * receive the oldest animal of that type). They cannot select which specific animal they would like. Create the data structure to
+ * maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog and dequeueCat. You may use the built-in
+ * LinkedList data structure
+ */
+public class Problem001DogCatQueue {
+}
