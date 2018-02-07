feb21fbbe8a91a28aca0f14224ad4bfbe7a8009a
Edwin Francis <24edwin@gmail.com>
2018-02-07 11:56:07 +0800

    Solution for check subtree

--- src/Ch_04_Trees_and_Graphs/Q4_10_Check_Subtree/QuestionA.java
+++ src/Ch_04_Trees_and_Graphs/Q4_10_Check_Subtree/QuestionA.java
@@ -1,21 +1,43 @@
 package Q4_10_Check_Subtree;
 
-
 import CtCILibrary.CtCILibrary.AssortedMethods;
 import CtCILibrary.CtCILibrary.TreeNode;
+import java.util.*;
 
 public class QuestionA {
-	
-	public static boolean containsTree(TreeNode t1, TreeNode t2) {
-		return false;
+
+	public static void convertToString(TreeNode t, StringBuilder treeString) {
+		if(t == null) {
+			treeString.append("X");
+			return;
+		}
+		else {
+			treeString.append(t.data);
+			convertToString(t.left,treeString);
+			convertToString(t.right,treeString);
+		}
+		
 	}
-	
+
+	public static boolean containsTree(TreeNode t1, TreeNode t2) {
+		StringBuilder t1String = new StringBuilder();
+		StringBuilder t2String = new StringBuilder();
+		
+		convertToString(t1,t1String);
+		convertToString(t2,t2String);
+		
+		if(t1String.indexOf(t2String.toString()) == -1) {
+			return false;
+		}
+		else 
+			return true;		
+	}
 
 	public static void main(String[] args) {
 		// t2 is a subtree of t1
-		int[] array1 = {1, 2, 1, 3, 1, 1, 5};
-		int[] array2 = {2, 3, 1};
-		
+		int[] array1 = { 1, 2, 1, 3, 1, 1, 5 };
+		int[] array2 = { 2, 3, 1 };
+
 		TreeNode t1 = AssortedMethods.createTreeFromArray(array1);
 		TreeNode t2 = AssortedMethods.createTreeFromArray(array2);
 
@@ -26,7 +48,7 @@ public class QuestionA {
 		}
 
 		// t4 is not a subtree of t3
-		int[] array3 = {1, 2, 3};
+		int[] array3 = { 1, 2, 3 };
 		TreeNode t3 = AssortedMethods.createTreeFromArray(array1);
 		TreeNode t4 = AssortedMethods.createTreeFromArray(array3);
 
@@ -35,6 +57,7 @@ public class QuestionA {
 		} else {
 			System.out.println("t4 is not a subtree of t3");
 		}
+
 	}
 
 }
